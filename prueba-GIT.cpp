d177857455477cd115d09a9cd1a4af1b31977e7b
BernyJordanC <97140912+BernyJordanC@users.noreply.github.com>
2022-01-05 07:42:55 -0500

    Update prueba-GIT.cpp

--- prueba-GIT.cpp
+++ prueba-GIT.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int main(int argc, char *argv[]) {
 	int a=5, b=20, c=70;
 	
-	cout<<"a = "<<a<<", b = "<<b<<", c= "<<c;
-	
+	cout<<"a = "<<a<<", b = "<<b<<", c= "<<c<<endl;
+	cout<<"agregado dsd GitHub.com";
 	
 	
 	
