f1af3f8cb5d378c3fb41ff6d836772710d750309
BernyJordanC <97140912+BernyJordanC@users.noreply.github.com>
2022-01-05 07:43:42 -0500

    Update prueba2-GIT.cpp

--- folder/prueba2-GIT.cpp
+++ folder/prueba2-GIT.cpp
@@ -3,7 +3,8 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
 	cout<<"Hola soy el segundo archivo"<<endl;
-	cout<<"Estoy agregando cualquier cosa";
+	cout<<"Estoy agregando cualquier cosa"<<endl;
+	cout<<"Agregando dsd Github también";
 	return 0;
 }
 
